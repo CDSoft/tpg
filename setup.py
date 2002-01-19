964093037fb4eecfa01bca4a28089aa6b065a851
Christophe Delord <cdsoft.fr>
2002-01-20 00:00:52 +0100

    * Version 0.1.4
    * tp.g:
    	Changed unindent function
    	Command line is now parsed by TPG

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(	name="TPG",
-		version="0.1.3",
+		version="0.1.4",
 		description="Toy Parser Generator is a recursive descendant parser generator for Python 2.2",
 		author="Christophe Delord",
 		author_email="christophe.delord@free.fr",
